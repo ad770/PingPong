5859f9e51e40566ec1496032ee2c946554027e65
ad770 <zuchowski.adrian@gmail.com>
2021-12-17 21:58:36 +0100

    Add object movements

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,88 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int x=-5;
+int y=-5;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::mainTimer(TObject *Sender)
+{
+        ball->Left +=x;
+        ball->Top +=y;
+
+        //odbicie od górnej sciany;
+        if (ball->Top-5 <= playground->Top) y=-y;
+        //odbicie od dolnej sciany;
+        if ((ball->Top + ball->Height+5) >= (playground->Top + playground->Height)) y=-y;
+
+        //koniec gry
+        if (ball->Left < player1->Left + player1->Width - 25 || ball->Left + ball->Width > player2->Left + 15){
+                main->Enabled=false;
+                ball->Visible=false;
+                if (Application->MessageBoxA("Chcesz rozpoczac nowa gre?","Koniec gry", MB_YESNO | MB_ICONQUESTION) == IDYES) {
+                        ball->Top = 280; ball->Left = 496;
+                        ball->Visible=true;
+                        main->Enabled=true;
+                };
+        }
+        //odbicie player1
+        else if (ball->Top < player1->Top + player1->Height &&
+                ball->Top + ball->Height > player1->Top &&
+                ball->Left < player1->Left + player1->Width) {
+                        x=-x;
+                }
+
+        //odbicie player2
+        else if (ball->Top < player2->Top + player2->Height &&
+                ball->Top + ball->Height > player2->Top &&
+                ball->Left + ball->Width > player2->Left) {
+                        x=-x;
+                }
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::p1_upTimer(TObject *Sender)
+{
+        if (player1->Top > 10) player1->Top -= 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::p1_downTimer(TObject *Sender)
+{
+        if (player1->Top + player1->Height < playground->Top + playground->Height - 10) player1->Top += 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::p2_upTimer(TObject *Sender)
+{
+        if (player2->Top > 10) player2->Top -= 10;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::p2_downTimer(TObject *Sender)
+{
+        if (player2->Top + player2->Height < playground->Top + playground->Height - 10) player2->Top += 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if (Key == 0x41) p1_up->Enabled=true;
+        if (Key == 0x5A) p1_down->Enabled=true;
+        if (Key == VK_UP) p2_up->Enabled=true;
+        if (Key == VK_DOWN) p2_down->Enabled=true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        if (Key == 0x41) p1_up->Enabled=false;
+        if (Key == 0x5A) p1_down->Enabled=false;
+        if (Key == VK_UP) p2_up->Enabled=false;
+        if (Key == VK_DOWN) p2_down->Enabled=false;
+}
+//---------------------------------------------------------------------------
+
--- Unit1.h
+++ Unit1.h
@@ -13,11 +13,24 @@
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
-        TShape *Shape1;
+        TShape *playground;
         TShape *player1;
         TShape *player2;
-        TImage *Image1;
-        TTimer *Timer1;
+        TImage *ball;
+        TTimer *main;
+        TTimer *p1_up;
+        TTimer *p1_down;
+        TTimer *p2_up;
+        TTimer *p2_down;
+        void __fastcall mainTimer(TObject *Sender);
+        void __fastcall p1_upTimer(TObject *Sender);
+        void __fastcall p1_downTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall p2_upTimer(TObject *Sender);
+        void __fastcall p2_downTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
